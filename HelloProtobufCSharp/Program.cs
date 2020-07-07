e9f7ddda6178b599e32935be8e3a133f990d003a
Anthony Sneed <anthony.sneed@hilti.com>
2020-07-06 21:55:05 -0500

    Add code to Program.

--- HelloProtobufCSharp/Program.cs
+++ HelloProtobufCSharp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using HelloProtobufCSharp.v1.Models;
+using Google.Protobuf.WellKnownTypes;
+using System.IO;
+using Google.Protobuf;
 
 namespace HelloProtobufCSharp
 {
@@ -6,7 +10,30 @@ namespace HelloProtobufCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Storage
+            byte[] bytes;
+
+            // New weather
+            var weather = new WeatherData
+            {
+                Summary = "Sunny",
+                TemperatureF = 70,
+                DateTimeStamp = Timestamp.FromDateTime(DateTime.UtcNow)
+            };
+
+            // Save to buffer
+            using (var memoryStream = new MemoryStream())
+            {
+                weather.WriteTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+            // Copy from buffer
+            var weatherCopy = WeatherData.Parser.ParseFrom(bytes);
+
+            // Perform deep-comparison on equality
+            var areEqual = weather.Equals(weatherCopy);
+            Console.WriteLine($"Original Weather and copy are equal: {areEqual}");
         }
     }
 }
